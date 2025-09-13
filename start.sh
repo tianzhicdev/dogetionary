00c976c0e8a9c590a27a010ccc1cfef975b1262f
tianzhicdev <tianzhic.dev@gmail.com>
2025-09-13 17:51:16 -0400

    almost perfect

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Stopping any existing services..."
-docker-compose down
+echo "Stopping app and nginx services (keeping database running)..."
+docker-compose stop app nginx
+docker-compose rm -f app nginx
 
-echo "Building and starting services..."
-docker-compose up --build -d
+echo "Building and starting app and nginx services..."
+docker-compose up --build -d app nginx
 
 echo "Dictionary app is now running on http://localhost:5000"
 echo "Test with: curl 'http://localhost:5000/word?w=hello'"
